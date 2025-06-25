7bc8a2746c8c21263d549939140c4b46a60d7078
Sholto Maud <sholto.maud@gmail.com>
2025-06-25 13:44:36 +1000

    Initial Commit

---
+++ Flume AppTests/Flume_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Flume_AppTests.swift
+//  Flume AppTests
+//
+//  Created by Sholto Maud on 25/6/2025.
+//
+
+import Testing
+
+struct Flume_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
